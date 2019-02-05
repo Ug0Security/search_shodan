82f6e93b6f38ee27d476b76781d70119eb7c8c17
Ug0Security <c.vallicari@gmail.com>
2019-02-05 10:00:28 +0100

    Create search_ip.py

---
+++ search_ip.py
@@ -0,0 +1,33 @@
+import shodan
+import sys
+
+SHODAN_API_KEY = "SHODAN_API_KEY"
+
+api = shodan.Shodan(SHODAN_API_KEY)
+PAGE = sys.argv[1]
+PAGE2 = int(PAGE) + 1
+try:
+        # Search Shodan
+        results = api.search('port:111 nfs country:fr', page=PAGE)
+
+        # Show the results
+        print 'Results found: %s' % results['total']
+                
+        for result in results['matches']:
+                print result['ip_str'].rstrip()
+ 
+except shodan.APIError, e:
+        print 'Error: %s' % e
+
+try:
+        # Search Shodan
+        results = api.search('port:111 nfs country:fr', page=PAGE2)
+
+      
+       
+                
+        for result in results['matches']:
+                print result['ip_str'].rstrip() 
+ 
+except shodan.APIError, e:
+        print 'Error: %s' % e
